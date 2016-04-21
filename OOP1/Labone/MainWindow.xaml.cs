9b6888467c444f48a1f79e989d058d469acf0ef2
SmallYellowTangerine <anastas.ego@gmail.com>
2016-04-21 19:38:50 +0300

    check

---
+++ OOP1/Labone/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Labone
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Sh1 line1 = new Sh1();
+            line1.drawLine(myCanvas, 40, 40, 100, 100);
+            Sh2 triangle1 = new Sh2();
+            triangle1.drawTriangle(myCanvas, 150, 40, 250, 40);
+            Sh3 some4angle1 = new Sh3();
+            some4angle1.draw4angle(myCanvas, 300, 40, 350, 40);
+            
+        }
+
+        
+
+       
+    }
+}
---
+++ OOP1/Labone/Sh2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Labone
+{
+    class Sh2 : Sh1
+    {
+        public void drawTriangle(Canvas canv, int xcoor1, int ycoor1, int xcoor2, int ycoor2)
+        {
+
+            drawLine(canv, xcoor1, ycoor1, xcoor2, ycoor2);
+            drawLine(canv, xcoor2, ycoor2, xcoor1+30, ycoor1+30);
+            drawLine(canv, xcoor1 + 30, ycoor1 + 30, xcoor1, ycoor1);
+        }
+    }
+}
